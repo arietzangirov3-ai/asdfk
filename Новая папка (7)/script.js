ad1f29701da034250602bdd5517b76afba531b4b
arietzangirov3-ai <arietzangirov3@gmail.com>
2025-11-27 22:00:17 +0600

    Add files via upload

---
+++ Новая папка (7)/script.js
@@ -0,0 +1,12 @@
+
+const tekshere = () => {
+    let malymat = prompt("Маалымат жазыңыз:");
+
+    if (malymat === "admin") {
+        alert("Кош келиңиз, сайттын ээси!");
+    } else {
+        alert("Кош келиңиз, биздин сайтка!");
+    }
+};
+
+tekshere();
